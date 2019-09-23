ea8742b01eed8068b985b83ee059bfc585ea5959
Manoj Soni <er.manojsoni@hotmail.com>
2019-09-23 09:10:50 +0530

    added float format specifier

--- formatspecifiers/FloatFormatSpecifiersAnalysis.c
+++ formatspecifiers/FloatFormatSpecifiersAnalysis.c
@@ -4,43 +4,45 @@ int main() {
 // RJ = Right Justified, LJ = Left Justified, B = BLOCKS
 
 float a = 8.358416, b = 35.434565, c = 134.456576, d = 2654.453456, e= 12345.678999, f= 123456.789999, g= 1234567.899999, h= 1234567.8999999;
-//char abc[] = "0.2f";
-//printf("%s", abc[]);
+// LJ Upto 2 decimal places
 printf("%0.2f\n", a); // LJ |8|.|3|6|_|_| // As-Is + LJ + Rounded upto 2 Decimal Places
-printf("%0.2f\n", b); // LJ |3|5|.|4|3|_| // LJ coz of 0.*
+printf("%0.2f\n", b); // LJ |3|5|.|4|3|_| // 
 printf("%0.2f\n", c); // LJ |1|3|4|.|4|6|
 printf("%0.2f\n\n", d); // LJ |2|6|5|4|.|4|5|
 
-
+// Won't understand the minus, therfore, LJ upto 2 decimal places
 printf("%-6.2f\n", a); // LJ |8|.|3|6|_|_| // LJ + 6B + Rounded upto 2 Decimal Places
-printf("%-6.2f\n", b); // LJ |3|5|.|4|3|_| // LJ coz of minus(-) sign
+printf("%-6.2f\n", b); // LJ |3|5|.|4|3|_| // 
 printf("%-6.2f\n", c); // LJ |1|3|4|.|4|6| // Rounded off upto two-decimal places
 printf("%-6.2f\n\n", d); // LJ |2|6|5|4|.|4|5|
 
-//new line
-printf("%6f\n", a); // LJ |_|_|_|_|+|2| // 6B + PLUS SIGN included in blocks + RJ
-printf("%6f\n", b); // LJ |_|_|_|+|2|8| // includes plus Sign coz of PLUS SIGN
-printf("%6f\n", c); // LJ |_|_|+|1|0|0|
-printf("%6f\n\n", d); // LJ |_|+|4|0|0|0|
+// LJ upto 6 decimal places
+printf("%6f\n", a);
+printf("%6f\n", b);
+printf("%6f\n", c); 
+printf("%6f\n\n", d); 
 
-printf("%+6f\n", a); // LJ |_|_|_|_|+|2| // 6 blocks + PLUS SIGN included in blocks + Right Justified
-printf("%+6f\n", b); // LJ |_|_|_|+|2|8| // includes plus Sign coz of PLUS SIGN
-printf("%+6f\n", c); // LJ |_|_|+|1|0|0|
-printf("%+6f\n\n", d); // LJ |_|+|4|0|0|0|
+// LJ UPTO 6 DECIMAL PLACES WITH + SIGN IN THE FRONT
+printf("%+6f\n", a); 
+printf("%+6f\n", b); 
+printf("%+6f\n", c); 
+printf("%+6f\n\n", d); 
 
-//new line
-printf("%6.2f\n", a); // RJ |_|_|_|_|+|2| // 6B & +' included in blocks + RJ
-printf("%6.2f\n", b); // RJ |_|_|_|+|2|8| // includes plus Sign coz of PLUS SIGN
-printf("%6.2f\n", c); // RJ |_|_|+|1|0|0|
-printf("%6.2f\n\n", d); // RJ |_|+|4|0|0|0|
+// CASE OF RIGHT JUSTIFIED
+// %X.Y --> The Real Case, X = NO OF CONSIDERING BLOCKS,
+// Y = NO. OF DECIMAL PLACES FROM RIGHT TO LEFT IN THAT X BLOCKS
+printf("%6.2f\n", a); 
+printf("%6.2f\n", b);
+printf("%6.2f\n", c); 
+printf("%6.2f\n\n", d); 
 
-//new line
+// Wont understand minus, therefore LJ, upto 6 decimal places
 printf("%-6f\n", a);
 printf("%-6f\n", b);
 printf("%-6f\n", c);
 printf("%-6f\n\n", d);
 
-//new line
+// CASE OF RJ, WITH 7 BLOCKS WITH UPTO DIFFERENT DECIMAL PLACES
 printf("%7.2f\n", a); 
 printf("%7.3f\n", b); 
 printf("%7.4f\n", c); 
@@ -50,7 +52,7 @@ printf("%7.7f\n", b);
 printf("%7.8f\n", c); 
 printf("%7.9f\n\n", d);
 
-//new line
+// CASE OF RJ, WITH 8 BLOCKS WITH UPTO DIFFERENT DECIMAL PLACES
 printf("%8.2f\n", a); 
 printf("%8.3f\n", a); 
 printf("%8.4f\n", a); 
@@ -60,7 +62,7 @@ printf("%8.7f\n", a);
 printf("%8.8f\n", a); 
 printf("%8.9f\n\n", a);
 
-//new line
+// CASE OF RJ, WITH 9 BLOCKS WITH UPTO DIFFERENT DECIMAL PLACES
 printf("%9.2f\n", b); 
 printf("%9.3f\n", b); 
 printf("%9.4f\n", b); 
@@ -70,7 +72,7 @@ printf("%9.7f\n", b);
 printf("%9.8f\n", b); 
 printf("%9.9f\n\n", b);
 
-//new line
+// CASE OF RJ, WITH 10 BLOCKS WITH UPTO DIFFERENT DECIMAL PLACES
 printf("%10.2f\n", c); 
 printf("%10.3f\n", c); 
 printf("%10.4f\n", c); 
@@ -80,7 +82,7 @@ printf("%10.7f\n", c);
 printf("%10.8f\n", c); 
 printf("%10.9f\n\n", c);
 
-//new line
+// CASE OF RJ, WITH 11 BLOCKS WITH UPTO DIFFERENT DECIMAL PLACES
 printf("%11.2f\n", d); 
 printf("%11.3f\n", d); 
 printf("%11.4f\n", d); 
@@ -90,45 +92,45 @@ printf("%11.7f\n", d);
 printf("%11.8f\n", d); 
 printf("%11.9f\n\n", d);
 
-//new line
-printf("%11.2f\n", e); 
-printf("%11.3f\n", e); 
-printf("%11.4f\n", e); 
-printf("%11.5f\n", e);
-printf("%11.6f\n", e); 
-printf("%11.7f\n", e); 
-printf("%11.8f\n", e); 
-printf("%11.9f\n\n", e);
+// CASE OF RJ, WITH 12 BLOCKS WITH UPTO DIFFERENT DECIMAL PLACES
+printf("%12.2f\n", e); 
+printf("%12.3f\n", e); 
+printf("%12.4f\n", e); 
+printf("%12.5f\n", e);
+printf("%12.6f\n", e); 
+printf("%12.7f\n", e); 
+printf("%12.8f\n", e); 
+printf("%12.9f\n\n", e);
 
-//new line
-printf("%11.2f\n", f); 
-printf("%11.3f\n", f); 
-printf("%11.4f\n", f); 
-printf("%11.5f\n", f);
-printf("%11.6f\n", f); 
-printf("%11.7f\n", f); 
-printf("%11.8f\n", f); 
-printf("%11.9f\n\n", f);
+// CASE OF RJ, WITH 13 BLOCKS WITH UPTO DIFFERENT DECIMAL PLACES
+printf("%13.2f\n", f); 
+printf("%13.3f\n", f); 
+printf("%13.4f\n", f); 
+printf("%13.5f\n", f);
+printf("%13.6f\n", f); 
+printf("%13.7f\n", f); 
+printf("%13.8f\n", f); 
+printf("%13.9f\n\n", f);
 
-//new line
-printf("%11.2f\n", g); 
-printf("%11.3f\n", g); 
-printf("%11.4f\n", g); 
-printf("%11.5f\n", g);
-printf("%11.6f\n", g); 
-printf("%11.7f\n", g); 
-printf("%11.8f\n", g); 
-printf("%11.9f\n\n", g);
+// CASE OF RJ, WITH 14 BLOCKS WITH UPTO DIFFERENT DECIMAL PLACES
+printf("%14.2f\n", g); 
+printf("%14.3f\n", g); 
+printf("%14.4f\n", g); 
+printf("%14.5f\n", g);
+printf("%14.6f\n", g); 
+printf("%14.7f\n", g); 
+printf("%14.8f\n", g); 
+printf("%14.9f\n\n", g);
 
-//new line
-printf("%11.2f\n", h); 
-printf("%11.3f\n", h); 
-printf("%11.4f\n", h); 
-printf("%11.5f\n", h);
-printf("%11.6f\n", h); 
-printf("%11.7f\n", h); 
-printf("%11.8f\n", h); 
-printf("%11.9f\n\n", h);
+// CASE OF RJ, WITH 15 BLOCKS WITH UPTO DIFFERENT DECIMAL PLACES
+printf("%15.2f\n", h); 
+printf("%15.3f\n", h); 
+printf("%15.4f\n", h); 
+printf("%15.5f\n", h);
+printf("%15.6f\n", h); 
+printf("%15.7f\n", h); 
+printf("%15.8f\n", h); 
+printf("%15.9f\n\n", h);
 
 
 return 0;
