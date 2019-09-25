21ad97ac7211270c1ad83d200a0cff907e521ddd
Manoj Soni <er.manojsoni@hotmail.com>
2019-09-26 01:50:13 +0530

    added pointers

---
+++ pointers/pointers.c
@@ -0,0 +1,22 @@
+//***************************************************************---C76-->|
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Created by Manoj Soni on Thursday, September 26, 2019  */
+/* Description: Program Description  */
+
+int main() {
+  
+  /*Code here*/
+  
+  
+  
+  
+  getch();
+  return 0;
+}
+
+//** L49, A4 Paper ends, leave L50 empty
+
+//***************************************************************---C76-->|
