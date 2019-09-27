3e37162d7219b086745fb598437a8be31fd75b6f
Manoj Soni <er.manojsoni@hotmail.com>
2019-09-27 10:19:53 +0530

    added dynamic memory allocation

---
+++ pointers/dynamic-memory-allocation-malloc.c
@@ -0,0 +1,52 @@
+//***************************************************************---C76-->|
+#include <stdio.h>
+#include <stdlib.h> // Required for Malloc();
+
+/* Created by Manoj Soni on Friday, September 27, 2019  */
+/* Description: Dynamic Memory Allocation - Malloc()  */
+int main() {
+  
+  int *p; // int* p; this is also true, but when you declare more than one
+  // integer pointer, use this
+
+  // ask user for no. of elements
+  int n;// i; //sum=0;
+  printf("Enter no of elements:\n");
+  scanf("%d", &n);
+  // Dynamically allocate memory using malloc()
+  // Syntax: (byte-size) = X multiplied by the sizeof(data-type)
+  // Syntax: ptr = (cast-type*) malloc (byte-size)
+  p = (int*) malloc (n * sizeof(int));
+  
+  // Check if the memory has been successfully
+  // allocated by malloc or not
+  if (p == NULL){
+    //tab to code
+   printf("Memory not allocated.\n");
+   exit(0); // exit the program afteer displaying that
+  } else {
+       printf("Memory allocated successfully.\n");
+       
+       // get the elements of the array
+       // ++i is faster
+       for (int i=0; i<n; ++i) { // i=0 only initialized once
+        p[i]= i+1; // after init --> CPU 1: Condition check, 
+        // 2: Process statements, 3:Update
+        
+       }
+       
+       // Print the elements of the array
+       // ++i is faster
+       printf("The elements of the array are: ");
+       for (int i=0; i<n; ++i) { // i=0 only initialized once 
+        printf("%d, ", p[i]); 
+        
+       }
+  }
+  getch();
+  return 0;
+}
+
+//** L49, A4 Paper ends, leave L50 empty
+
+//***************************************************************---C76-->|
